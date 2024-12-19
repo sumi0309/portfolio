4ef0e090ec4de760f768775df5ddc29c2fdfe644
Sumiran Jaiswal <sumi0309@umd.edu>
2024-12-18 21:07:07 -0500

    Initial webpage

---
+++ script.js
@@ -0,0 +1,19 @@
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  navLinks.forEach((link) => {
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      const targetId = link.getAttribute("href").slice(1);
+      const targetSection = document.getElementById(targetId);
+
+      if (targetSection) {
+        window.scrollTo({
+          top: targetSection.offsetTop - 50,
+          behavior: "smooth",
+        });
+      }
+    });
+  });
+});
